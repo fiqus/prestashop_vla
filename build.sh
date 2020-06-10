016ee3d12fcb1782418a63c25569bb47fc77ce0d
Martin Silva <martinsilva@fiqus.com>
2020-06-10 16:31:07 -0300

    dev env

--- build.sh
+++
@@ -1,3 +0,0 @@
-docker network create prestashop-net
-docker run -ti --name prestashop_db --network prestashop-net -e MYSQL_ROOT_PASSWORD=admin -p 3307:3306 -d mysql:5.7
-docker run -ti --name prestashop_web --network prestashop-net -e DB_SERVER=prestashop_db -p 8080:80 -d prestashop/prestashop
