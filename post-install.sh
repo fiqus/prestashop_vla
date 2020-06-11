0fb12d3e6f9163c8691df023c59031518447ca50
Martin Silva <martinsilva@fiqus.com>
2020-06-11 16:48:18 -0300

    improve post install script

--- post-install.sh
+++ post-install.sh
@@ -3,16 +3,11 @@
 if [[ -z "${ADMIN_PASS}" ]]; then
   echo "You need to provide a new admin pass"
 else
-    ADMIN_DIR="/var/www/html/admin"
-    if [ -d "$ADMIN_DIR" ]; then
-      echo "Deleting install folder and moving admin folder to: ${ADMIN_DIR}..."
-      docker exec economia_social_vla_prestashop_web_1 rm -rf /var/www/html/install
-      docker exec economia_social_vla_prestashop_web_1 mv ${ADMIN_DIR} ${ADMIN_DIR}${ADMIN_PASS}
+  ADMIN_DIR="/var/www/html/admin"
+  echo "Deleting install folder and moving admin folder to: ${ADMIN_DIR}..."
+  docker exec economia_social_vla_prestashop_web_1 rm -rf /var/www/html/install
+  docker exec economia_social_vla_prestashop_web_1 mv ${ADMIN_DIR} ${ADMIN_DIR}${ADMIN_PASS}
 
-      echo "OK. You can enter the admin console in /admin{ADMIN_PASS}"
-    else
-      echo "Error: ${ADMIN_DIR} not found. Can not continue."
-      exit 1
-    fi
+  echo "OK. You can enter the admin console in /admin{ADMIN_PASS}"
 fi
 
